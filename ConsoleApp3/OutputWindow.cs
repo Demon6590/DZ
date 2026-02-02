b14358b972a52e5483a9239337b402b25d41860b
Demon6590 <dima.chernytin@gmail.com>
2026-02-02 15:00:49 +0300

    первая попытка сдать задание

---
+++ ConsoleApp3/OutputWindow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp3;
+using Terminal.Gui.App;
+using Terminal.Gui.Views;
+
+
+
+public class OutputWindow: Runnable
+{
+    private string path = "Info";
+    List<string> info = new List<string>();
+    public OutputWindow()
+    {
+        Title = $"Example App ({Application.QuitKey} to quit)";
+        
+        foreach(string lins in File.ReadLines(path))
+        {
+            info.Add(lins);
+        }        
+
+        var label = new Label()
+        {
+ 
+            Text = $"{string.Join("\n", info)}"
+        };
+        
+        Add(label);
+    }
+}
